4d2bbba9fba09f14ea11c9e84d2d0c6e433e88dc
Julia K <korchkova.yulia@gmail.com>
2024-09-24 19:48:16 +0300

    test: jest with configs n update general workflow
      - Added the following packages: jest, jest-environment-jsdom,
    testing-library/react, testing-library/dom, testing-library/jest-dom
      - Added jest.config and jest.setup files
      - Updated general workflow to include jest tests
      - Added test scripts to package.json

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  collectCoverage: true,
+  collectCoverageFrom: ['app/**/*.ts'],
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);

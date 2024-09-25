7547554d654113c36f4426bd887b4f2cc87fbfb0
Julia K <korchkova.yulia@gmail.com>
2024-09-25 20:34:42 +0300

    feat: add toPascalCaseWithSpaces func n lib alias
      - Updated tsconfig to include lib alias
      - Added toPascalCaseWithSpaces format function to change string to pascal case
    text with spaces

---
+++ lib/format/toPascalCaseWithSpaces.ts
@@ -0,0 +1,6 @@
+export const toPascalCaseWithSpaces = (str: string) =>
+  str
+    .toLowerCase()
+    .replace(/[-_]+/g, ' ')
+    .replace(/[^\w\s]/g, '')
+    .replace(/\b\w/g, (char) => char.toUpperCase());

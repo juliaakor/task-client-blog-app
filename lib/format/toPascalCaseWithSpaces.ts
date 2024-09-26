df5c372e4c0046c41a7f508d203d38fe09122be5
Julia K <korchkova.yulia@gmail.com>
2024-09-26 13:23:11 +0300

    refactor: toPascalCaseWithSpaces implementation
      - Updated toPascalCaseWithSpaces implementation to handle leading and
    trailing spaces as well as remove special characters

--- lib/format/toPascalCaseWithSpaces.ts
+++ lib/format/toPascalCaseWithSpaces.ts
@@ -2,5 +2,6 @@ export const toPascalCaseWithSpaces = (str: string) =>
   str
     .toLowerCase()
     .replace(/[-_]+/g, ' ')
-    .replace(/[^\w\s]/g, '')
-    .replace(/\b\w/g, (char) => char.toUpperCase());
+    .replace(/[^\w\s]+/g, ' ')
+    .replace(/\b\w/g, (char) => char.toUpperCase())
+    .trim();

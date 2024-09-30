82789ea5cecd1267a4172c2b8ead97d3776c7ac9
Julia K <korchkova.yulia@gmail.com>
2024-09-30 15:49:01 +0300

    fix: move layout due to DYNAMIC_SERVER_USAGE err

--- app/[locate]/layout.tsx
+++ app/[locate]/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
 import { Sen } from 'next/font/google';
 import { NextIntlClientProvider } from 'next-intl';
-// eslint-disable-next-line camelcase
-import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
+import { getMessages } from 'next-intl/server';
 
 import '@app/globals.css';
+
+import { LayoutProps } from './types';
 
 const sen = Sen({
   display: 'swap',
@@ -16,18 +17,11 @@ export const metadata: Metadata = {
   title: 'Client Blog',
 };
 
-export default async function RootLayout({
-  children,
-  params,
-}: Readonly<{
-  children: React.ReactNode;
-  params: { locale: string };
-}>) {
-  unstable_setRequestLocale(params.locale);
+export default async function RootLayout({ children, params: { locale } }: Readonly<LayoutProps>) {
   const messages = await getMessages();
 
   return (
-    <html lang="en" className={sen.className}>
+    <html lang={locale} className={sen.className}>
       <body className="antialiased bg-background text-foreground">
         <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
       </body>
--- app/[locate]/types.ts
+++ app/[locate]/types.ts
@@ -1,7 +1,12 @@
-interface Locate {
+export interface Locale {
   locale: string;
 }
 
+export interface LayoutProps {
+  children: React.ReactNode;
+  params: Locale;
+}
+
 export interface HomeProps {
-  params: Locate;
+  params: Locale;
 }

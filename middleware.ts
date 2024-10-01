a513da8b63175ac59583678cbe367920f3b51c2d
Julia K <korchkova.yulia@gmail.com>
2024-10-01 15:21:06 +0300

    feat: delete yup to add zod package n update api
      - Deleted yup package and added zod package
      - Updated middleware not to modify api requests in future

--- middleware.ts
+++ middleware.ts
@@ -6,8 +6,14 @@ import { getRedirectUrlForLocales } from '@lib/routing/getRedirectUrlForLocales'
 
 const handleI18nRouting = createMiddleware(routing);
 
+const API_PATH = '/api/';
+
 export default function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith(API_PATH)) {
+    return NextResponse.next();
+  }
 
   const redirectedUrl = getRedirectUrlForLocales(pathname, request);
   if (redirectedUrl) {

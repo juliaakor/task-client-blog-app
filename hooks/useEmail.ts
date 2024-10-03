b26f80f655cbf8a316a67370db288f4dfa927c3f
Julia K <korchkova.yulia@gmail.com>
2024-10-03 15:06:33 +0300

    feat: add useEmail hook to send all emails

---
+++ hooks/useEmail.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+
+import { ENV } from '@constants/env';
+import { sendEmail } from '@lib/email';
+import { FeedbackEmailForm, feedbackSchema, SubscriptionEmailForm, subscriptionSchema } from '@lib/zod/email';
+
+const templatesId = {
+  feedback: ENV.EMAILJS_TEMPLATE_ID_FEEDBACK,
+  subscription: ENV.EMAILJS_TEMPLATE_ID_SUBSCRIPTION,
+};
+
+const sendEmailToGmailService = (templateId: string, data: unknown, schema: z.ZodSchema) => {
+  sendEmail(ENV.EMAILJS_SERVICE_ID, templateId, ENV.EMAILJS_PUBLIC_KEY, data, schema);
+};
+
+export const useEmail = () => {
+  const sendFeedbackEmail = (data: FeedbackEmailForm) =>
+    sendEmailToGmailService(templatesId.feedback, data, feedbackSchema);
+
+  const sendSubscriptionEmail = (data: SubscriptionEmailForm) =>
+    sendEmailToGmailService(templatesId.subscription, data, subscriptionSchema);
+
+  return {
+    sendFeedbackEmail,
+    sendSubscriptionEmail,
+  };
+};

0abe4c57e3d06cb9c5cc430841c0ec9006c76f69
Julia K <korchkova.yulia@gmail.com>
2024-10-04 16:10:29 +0300

    feat: add formik and zod packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   plugins: [],
   theme: {
     extend: {

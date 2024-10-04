58e40cab5db7c62ce5747c9b382fbd844013ab51
Julia K <korchkova.yulia@gmail.com>
2024-10-04 07:46:41 +0300

    feat: add mocks for users and posts

---
+++ lib/mocks/users.ts
@@ -0,0 +1,123 @@
+import AndrewJonhsonAvatar from '@assets/authors/AndrewJonhson.png';
+import DianneRussellAvatar from '@assets/authors/DianneRussell.png';
+import EleanorPenaAvatar from '@assets/authors/EleanorPena.png';
+import FloydMilesAvatar from '@assets/authors/FloydMiles.png';
+import GuyHawkinsAvatar from '@assets/authors/GuyHawkins.png';
+import JacobJonesAvatar from '@assets/authors/JacobJones.png';
+import JennyWilsonAvatar from '@assets/authors/JennyWilson.png';
+import JohnDoeAvatar from '@assets/authors/JohnDoe.png';
+import LeslieAlexanderAvatar from '@assets/authors/LeslieAlexander.png';
+import RobertFoxAvatar from '@assets/authors/RobertFox.png';
+
+export default [
+  {
+    about:
+      'Sapiente esse praesentium omnis laudantium voluptatum. Placeat ipsam mollitia perferendis accusamus consequatur recusandae repellendus blanditiis ea. Error iste nesciunt eum modi. Odit itaque debitis at aut quibusdam reiciendis. Hic nostrum ut eligendi consectetur deleniti sint. Officia aspernatur dicta eaque harum explicabo maxime perferendis error.',
+    avatar: AndrewJonhsonAvatar,
+    company: 'company 1',
+    createdAt: '2024-09-26T22:33:51.317Z',
+    id: '1',
+    name: 'Andrew Jonhson',
+    role: 'role 1',
+    socials: {},
+  },
+  {
+    about:
+      'Quos deserunt possimus ea accusantium iure illum vel nesciunt fugiat. Eaque numquam nemo dicta id vero. Praesentium harum eveniet corrupti dolor itaque debitis rerum nam quos. Molestiae perferendis laudantium ad. Ducimus voluptas aperiam ipsa saepe explicabo quam delectus voluptatum.',
+    avatar: JohnDoeAvatar,
+    company: 'company 2',
+    createdAt: '2024-09-27T07:59:26.800Z',
+    id: '2',
+    name: 'John Doe',
+    role: 'role 2',
+    socials: {},
+  },
+  {
+    about:
+      'Soluta recusandae distinctio. Dolorem ullam expedita saepe quos dolore quos dicta in. Saepe sint necessitatibus. Perferendis necessitatibus odio nobis aut. Ex fugit distinctio quasi deleniti atque consequatur illo aspernatur nihil.',
+    avatar: FloydMilesAvatar,
+    company: 'company 3',
+    createdAt: '2024-09-27T10:55:28.510Z',
+    id: '3',
+    name: 'Floyd Miles',
+    role: 'role 3',
+    socials: {},
+  },
+  {
+    about:
+      'Nobis soluta sint est doloribus amet architecto facilis atque tempora. Ipsa facilis deleniti perspiciatis ut maiores. Eligendi voluptatem voluptatibus mollitia.',
+    avatar: DianneRussellAvatar,
+    company: 'company 4',
+    createdAt: '2024-09-26T13:03:46.670Z',
+    id: '4',
+    name: 'Dianne Russell',
+    role: 'role 4',
+    socials: {},
+  },
+  {
+    about:
+      'Omnis necessitatibus incidunt tempore. Repellat fugiat ea. Aut nisi assumenda recusandae unde consequatur qui debitis itaque. Sit id dolorum. Consectetur officia praesentium aspernatur architecto incidunt quam autem ad distinctio.',
+    avatar: JennyWilsonAvatar,
+    company: 'company 5',
+    createdAt: '2024-09-26T16:38:25.748Z',
+    id: '5',
+    name: 'Jenny Wilson',
+    role: 'role 5',
+    socials: {},
+  },
+  {
+    about:
+      'Et harum consectetur molestiae saepe aperiam eaque eum nulla optio. Distinctio dicta qui delectus odio accusamus impedit. Officiis dolorem distinctio quam sit quaerat optio. Possimus cumque dolorum quos sunt omnis fuga officia possimus molestias. Numquam sit odio delectus nesciunt rerum nisi quos distinctio. Consequuntur eum atque debitis earum voluptas itaque adipisci.',
+    avatar: LeslieAlexanderAvatar,
+    company: 'company 6',
+    createdAt: '2024-09-26T23:48:18.714Z',
+    id: '6',
+    name: 'Leslie Alexander',
+    role: 'role 6',
+    socials: {},
+  },
+  {
+    about:
+      'Harum non natus. Magni rem quidem repellat maiores laborum porro eligendi. Eligendi exercitationem commodi architecto consequatur dolorem aliquam. Laboriosam quos veniam accusamus ab occaecati modi neque sint. Provident molestias id nostrum debitis voluptatibus distinctio. Velit incidunt ut pariatur occaecati architecto saepe.',
+    avatar: GuyHawkinsAvatar,
+    company: 'company 7',
+    createdAt: '2024-09-27T02:25:33.627Z',
+    id: '7',
+    name: 'Guy Hawkins',
+    role: 'role 7',
+    socials: {},
+  },
+  {
+    about:
+      'Quisquam laborum earum deleniti occaecati quidem. Amet mollitia officiis voluptates. Blanditiis assumenda aspernatur officia incidunt officia ratione occaecati. Beatae ut rerum perferendis non laboriosam.',
+    avatar: EleanorPenaAvatar,
+    company: 'company 8',
+    createdAt: '2024-09-27T05:02:08.203Z',
+    id: '8',
+    name: 'Eleanor Pena',
+    role: 'role 8',
+    socials: {},
+  },
+  {
+    about:
+      'Harum non natus. Magni rem quidem repellat maiores laborum porro eligendi. Eligendi exercitationem commodi architecto consequatur dolorem aliquam. Laboriosam quos veniam accusamus ab occaecati modi neque sint. Provident molestias id nostrum debitis voluptatibus distinctio. Velit incidunt ut pariatur occaecati architecto saepe.',
+    avatar: RobertFoxAvatar,
+    company: 'company 7',
+    createdAt: '2024-09-27T02:25:33.627Z',
+    id: '9',
+    name: 'Robert Fox',
+    role: 'role 7',
+    socials: {},
+  },
+  {
+    about:
+      'Quisquam laborum earum deleniti occaecati quidem. Amet mollitia officiis voluptates. Blanditiis assumenda aspernatur officia incidunt officia ratione occaecati. Beatae ut rerum perferendis non laboriosam.',
+    avatar: JacobJonesAvatar,
+    company: 'company 8',
+    createdAt: '2024-09-27T05:02:08.203Z',
+    id: '10',
+    name: 'Jacob Jones',
+    role: 'role 8',
+    socials: {},
+  },
+];

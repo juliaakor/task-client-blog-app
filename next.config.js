9cf7f74382e8b9f05439f8aa81f60412969dbb3e
Julia K <korchkova.yulia@gmail.com>
2024-10-12 22:09:21 +0300

    build: add rooks htlm parser n update next config
      - Added the following packages: html-react-parse, rooks
      - Update next config to update images domains

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['s3-alpha-sig.figma.com'],
+  },
+};
 
 export default withNextIntl(nextConfig);

c8719af4a976471a079b3acf3230184c64af8d9f
Julia K <korchkova.yulia@gmail.com>
2024-10-14 00:04:17 +0300

    feat: add Blog page with mobile

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,10 +1,39 @@
-/* eslint-disable camelcase */
-import { unstable_setRequestLocale } from 'next-intl/server';
+import { getTranslations } from 'next-intl/server';
 
-import { HomeProps } from '@app/[locale]/types';
+import { getPostById } from '@api/getPostById';
+import { getUserById } from '@api/getUserById';
+import { AllPostsWithPagination } from '@components/AllPostsWithPagination';
+import { CategoryList } from '@components/CategoryList';
+import { FeaturedPost } from '@components/FeaturedPost';
+import { JoinSection } from '@components/JoinSection';
+import { ENV } from '@constants/env';
+import { PageContent } from '@lib/components/PageContent';
+import { Typography } from '@lib/components/Typography';
 
-export default function Blog({ params: { locale } }: HomeProps) {
-  unstable_setRequestLocale(locale);
+export default async function Blog() {
+  const t = await getTranslations('home');
+  const post = await getPostById('29f5beff-7847-4c76-8411-0cbb6e5a8114', ENV.NEXT_PUBLIC_BASE_URL);
+  const user = await getUserById(post.userId, ENV.NEXT_PUBLIC_BASE_URL);
 
-  return <main>Blog</main>;
+  return (
+    <>
+      <FeaturedPost post={{ ...post, user }} className="bg-white-03" buttonLinkTitle="Read More >" />
+      <PageContent>
+        <div>
+          <AllPostsWithPagination />
+        </div>
+
+        <div>
+          <Typography tag="h2" className="mb-12 text-left">
+            {t('sectionTitles.allCatagories')}
+          </Typography>
+          <CategoryList isFullInfo cardClassName="w-72" />
+        </div>
+
+        <div className="w-1/3 max-768:w-3/5 max-425:4/5 m-auto">
+          <JoinSection />
+        </div>
+      </PageContent>
+    </>
+  );
 }
